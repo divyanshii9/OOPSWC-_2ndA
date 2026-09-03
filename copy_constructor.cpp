a1ab5c371f9c25f2efda280a734096e6ba6e3a02
divyanshii9 <divyanshiibhatia9@gmail.com>
2026-09-03 09:45:15 +0530

    const copy

--- copy_constructor.cpp
+++ copy_constructor.cpp
@@ -1,29 +1,49 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// class student{
+// private:
+//     string name;
+//     int marks;
+// public:
+//     student(){
+//         cout<<"Default Constructor: ";
+//     }
+//     student(string a,int b){
+//         name=a;
+//         marks=b;
+//     }
+//     student(const student &s){
+//         name=s.name;
+//         marks=s.marks;
+//     }
+//     void show(){
+//         cout<<name;
+//         cout<<marks;
+//     }
+// };
+// int main(){
+//     student s1("vikas",90);
+//     student s2=s1;
+//     s1.show();
+//     s2.show();
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 class student{
 private:
-    string name;
-    int marks;
+    int rollno;
 public:
-    student(){
-        cout<<"Default Constructor: ";
+    student(int r){
+        rollno=r;
+
     }
-    student(string a,int b){
-        name=a;
-        marks=b;
-    }
-    student(const student &s){
-        name=s.name;
-        marks=s.marks;
-    }
-    void show(){
-        cout<<name;
-        cout<<marks;
+    void display() const{
+        cout<<rollno;
     }
 };
 int main(){
-    student s1("vikas",90);
-    student s2=s1;
-    s1.show();
-    s2.show();
+    student s1(89);
+    s1.display();
 }
